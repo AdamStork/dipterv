b845348d7de2a546a5a1deb51a12f2a8761c3dc9
Gólya Ádám <sastunter@gmail.com>
2019-11-16 19:06:50 +0100

    pyQt update

--- pyQt/stm.py
+++ pyQt/stm.py
@@ -5,10 +5,11 @@ from decimal import Decimal
 import serial
 import functional_test_pb2
 
-qtcreator_file  = "stm_gui.ui" # Enter file here.
+qtcreator_file  = "D:/Work/dipterv/pyQt/stm_gui.ui" # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
-ser = serial
+ser = serial.Serial()
+
 
 class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -17,22 +18,47 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.connect_button.clicked.connect(self.connect)
         self.command_button.clicked.connect(self.send_command)
+        self.close_button.clicked.connect(self.close_port)
     def connect(self):
         try:
+            global ser
             ser = serial.Serial(port='COM9', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=0)
-            is_port_open = 'Port open' 
-        except serial.serialutil.SerialException:         
-            is_port_open = 'Error opening port'
+            is_port_open = 'Port open'
+        except serial.serialutil.SerialException:
+            if ser.is_open:
+                is_port_open = 'Port is already open'
+            else:
+                is_port_open = 'Error opening port'
+        self.connection_output.setText(is_port_open)
+    def close_port(self):
+        if ser.is_open:
+            try:
+                ser.close()
+                is_port_open = 'Port closed'
+            except serial.serialutil.SerialException:
+                is_port_open = 'Error closing port'
+        else:
+            is_port_open = 'Port is not open'
         self.connection_output.setText(is_port_open)
     def send_command(self):
-        ser.write(pb)
+        global ser
+        cmd = functional_test_pb2.Command()
+        cmd.commandType = functional_test_pb2.CommandTypeEnum.I2C_test
+        pb = cmd.SerializeToString()
+        try:
+            ser.write(pb)
+            command_send_success = 'Command sent'
+        except serial.serialutil.SerialException:
+            if ser.is_open:
+                command_send_success = 'Error while sending command'
+            else:
+                command_send_success = 'Port is not open'
+        self.command_output.setText(command_send_success)
 
-def initialize_pb(void):
-    
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
     window.show()
     sys.exit(app.exec_())
-
