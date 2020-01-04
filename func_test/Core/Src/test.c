cc89cde481001ca73a76a356385a7d21b94092bc
Gólya Ádám <sastunter@gmail.com>
2020-01-04 22:34:17 +0100

    Core outsourcing

--- func_test/Core/Src/test.c
+++ func_test/Core/Src/test.c
@@ -4,17 +4,23 @@
  *  Created on: 2019. okt. 19.
  *      Author: Adam Golya
  */
-
+#include <string.h>
+#include <stdbool.h>
 #include "pb_encode.h"
 #include "pb_decode.h"
-#include "test.h"
 #include "usart.h"
+#include "test.h"
+#include "link_layer.h"
+#include "functional_test.pb.h"
 
 
-
-uint8_t buffer_string[128];
-uint8_t rx_buffer[2];
-
+bool frameReady = false;
+uint8_t receiveByte;
+uint8_t transmitByte;
+uint8_t receiveBuffer[128];
+uint8_t transmitBuffer[128];
+uint8_t receiveBufferLen;
+link_layer_t linkLayer;
 
 
 /** @brief 	Encode message
@@ -63,7 +69,71 @@ void buffer_init_zero(uint8_t* pBuffer, uint8_t pSize)
 }
 
 
+/** @brief 	Send out buffer via UART
+ *  @param	pBuffer: pointer to buffer
+ *  @param	pSize: size of buffer**/
 void buffer_send(uint8_t* pBuffer, uint8_t pSize)
 {
 	HAL_UART_Transmit(&huart2,pBuffer, pSize, HAL_MAX_DELAY);
+}
+
+
+/** @brief Receive command via UART, and perform chosen test **/
+void enter_processing_state(void)
+{
+	Command message_in = Command_init_zero;
+	Command message_out = Command_init_zero;
+	buffer_init_zero(receiveBuffer, sizeof(receiveBuffer));
+	bool messageDecodeSuccessful = false;
+	bool testToggle = false;
+
+	while (1){
+	  HAL_UART_Receive_DMA(&huart2,(uint8_t*)&receiveByte, 1);
+	  if(frameReady){
+		  frameReady = false;
+		  messageDecodeSuccessful = decode_message(receiveBuffer, &message_in);
+		  if(messageDecodeSuccessful){
+			  switch(message_in.commandType){
+			  case CommandTypeEnum_STOP_CURRENT_TEST:
+				  break;
+			  case CommandTypeEnum_ADC_test:
+				  break;
+			  case CommandTypeEnum_I2C_test:
+				  // perif_init()
+				  // perif_test()
+				  // perif_uninit()
+				  break;
+			  case CommandTypeEnum_SPI_test:
+				  break;
+			  case CommandTypeEnum_LED_test:
+				  HAL_GPIO_TogglePin(LD2_GPIO_Port,LD2_Pin);
+				  if(testToggle){
+					  message_out.commandType = CommandTypeEnum_LED_test;
+					  testToggle = false;
+				  }
+				  else{
+					  message_out.commandType = CommandTypeEnum_GPIO_test;
+					  testToggle = true;
+				  }
+				  encode_message(transmitBuffer,&message_out);
+				  link_set_phy_write_fn(&linkLayer,&buffer_send);
+				  link_write(&linkLayer,transmitBuffer,strlen((char*)transmitBuffer));
+				  // transmitByte --> protobuf --> framing --> protobuf
+	//				  HAL_UART_Transmit_DMA(&huart2,,1);
+				  break;
+			  case CommandTypeEnum_GPIO_test:
+				  break;
+			  case CommandTypeEnum_TIMER_SINGLE_SHOT:
+				  break;
+			  case CommandTypeEnum_RTC_test:
+				  break;
+			  case CommandTypeEnum_PWM:
+				  break;
+			  default:
+				  break;
+			  }
+		  }
+	  }
+	}
+
 }
